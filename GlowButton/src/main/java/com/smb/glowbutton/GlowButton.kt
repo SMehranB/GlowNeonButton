2daf53377bab2def77e51d4a7216311b01badeca
SMehranB <m.behbahani@live.com>
2021-04-07 14:26:38 +0300

    Version 1.0 pre-release

--- GlowButton/src/main/java/com/smb/glowbutton/GlowButton.kt
+++ GlowButton/src/main/java/com/smb/glowbutton/GlowButton.kt
@@ -8,47 +8,19 @@ import android.content.Context
 import android.content.res.TypedArray
 import android.graphics.*
 import android.util.AttributeSet
-import android.util.Log.d
-import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 import android.view.animation.DecelerateInterpolator
+import androidx.annotation.ColorInt
+import androidx.annotation.FontRes
+import androidx.annotation.StyleRes
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.ColorUtils
-import java.util.*
 
-class GlowButton(context: Context, attrSet: AttributeSet?) : View(context, attrSet) {
+class GlowButton @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null): View(context, attributeSet) {
 
-    private val EXTRA_PADDING_WIDTH = 100
-    private val EXTRA_PADDING_HEIGHT = 25
-
-    //BACKGROUND PROPERTIES
-    private val backgroundPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private var backgroundRectF = RectF()
-    private var backgroundPadding = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16f, resources.displayMetrics)
-    private var glowPadding = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16f, resources.displayMetrics)
-    var cornerRadius: Float = 0f
-    var backColor: Int = Color.GREEN
-    var glowColor: Int = Color.GREEN
-
-    //RIPPLE EFFECT PROPERTIES
-    private val ripplePaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private lateinit var rippleGradient : RadialGradient
-    private var mRippleRadius = 100f
-    private var mRippleAlpha: Int = 0
-    var rippleColor: Int = 0
-    var rippleEnabled = true
-
-    //TEXT PROPERTIES
-    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.SUBPIXEL_TEXT_FLAG)
-    private var mTextAlpha: Int = 255
-    private var mTextColorOriginal: Int = 0
-    private var mTextX: Float = 0f
-    private var mTextY: Float = 0f
-    private var mTextSize: Float = 0f
-    var textColor: Int = 0
-    var textAllCaps: Boolean = true
-    var text: String = "GLOW BUTTON"
-    var textStyle = 0 //Normal Text (1 = Bold, 2 = Italic, 3 = bold_italic)
+    private val horizontalTextMargin: Float = dpToPixel(24)
+    private val verticalTextMargin: Float = dpToPixel(16)
 
     private val maskPath = Path()
 
@@ -59,123 +31,188 @@ class GlowButton(context: Context, attrSet: AttributeSet?) : View(context, attrS
     private var rippleAnimatorSet: AnimatorSet? = null
     private var enableDisableAnimatorSet: AnimatorSet? = null
 
-    var animationDuration: Long = 500L
+    //BACKGROUND PROPERTIES
+    private val backgroundPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var backgroundRectF = RectF()
+    private var backgroundPadding: Float = dpToPixel(16)
+    private var glowRadius: Float = dpToPixel(16)
+    var glowAnimationDuration: Long = 500L
+
+    var cornerRadius: Float = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    @ColorInt
+    var backColor: Int = Color.GREEN
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    @ColorInt
+    var glowColor: Int = Color.GREEN
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    //RIPPLE EFFECT PROPERTIES
+    private val ripplePaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private lateinit var rippleGradient: RadialGradient
+    private var mRippleRadius: Float = 0f
+    private var mRippleAlpha: Int = 0
+    var rippleAnimationDuration: Long = 1500
+    @ColorInt
+    var rippleColor: Int = 0
+    var rippleEnabled: Boolean = true
+
+    //TEXT PROPERTIES
+    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.SUBPIXEL_TEXT_FLAG)
+    private var mTextAlpha: Int = 255
+    private var mTextColorOriginal: Int = 0
+    private var mTextX: Float = 0f
+    private var mTextY: Float = 0f
+    private var mTextSize: Float = 0f
+    private var mTextColor: Int = 0
+
+    var text: String = "GLOW BUTTON"
+        set(value) {
+            field = value
+            requestLayout()
+        }
+
+    @StyleRes
+    var textStyle: Int = Typeface.NORMAL
+        set(value) {
+            field = value
+            requestLayout()
+        }
+
+    @FontRes
+    var textFont: Int = 0
+        set(value) {
+            field = value
+            requestLayout()
+        }
+
 
     init {
 
-        val attr : TypedArray = context.theme.obtainStyledAttributes(attrSet, R.styleable.GlowButton,0, 0)
+        val attr : TypedArray = context.theme.obtainStyledAttributes(attributeSet, R.styleable.GlowButton, 0, 0)
 
         with(attr){
-            cornerRadius = getDimension(R.styleable.GlowButton_gb_cornerRadius, resources.getDimension(R.dimen.cornerRadius))
+
+            //retrieving background attributes
+            cornerRadius = getDimension(R.styleable.GlowButton_gb_cornerRadius, dpToPixel(30))
             backColor = getInteger(R.styleable.GlowButton_gb_backgroundColor, Color.GREEN)
-
             glowColor = getInteger(R.styleable.GlowButton_gb_glowColor, Color.GREEN)
+            glowAnimationDuration = getInteger(R.styleable.GlowButton_gb_glowAnimationDuration, 500).toLong()
 
+            //retrieving ripple effect attributes
             rippleColor = getInteger(R.styleable.GlowButton_gb_rippleColor, ColorUtils.blendARGB(backColor, Color.BLACK, 0.5f))
+            rippleAnimationDuration = getInteger(R.styleable.GlowButton_gb_rippleAnimationDuration, 1500).toLong()
             rippleEnabled = getBoolean(R.styleable.GlowButton_gb_rippleEnabled, true)
 
+            //retrieving text attributes
             textStyle = getInt(R.styleable.GlowButton_android_textStyle, Typeface.NORMAL)
             mTextSize = getDimension(R.styleable.GlowButton_android_textSize, resources.getDimension(R.dimen.text_size))
-            textColor = getInteger(R.styleable.GlowButton_android_textColor, Color.BLACK)
-            textAllCaps = getBoolean(R.styleable.GlowButton_android_textAllCaps, true)
-            mTextColorOriginal = textColor
+            mTextColor = getInteger(R.styleable.GlowButton_android_textColor, Color.BLACK)
+            textFont = getResourceId(R.styleable.GlowButton_android_fontFamily, 0)
 
-            val textString = getString(R.styleable.GlowButton_text)
-            if (textString != null) {
-                text = if(textAllCaps){
-                    textString.toUpperCase(Locale.ROOT)
-                }else{
-                    textString
-                }
-            }
+            mTextColorOriginal = mTextColor
+
+            text = getString(R.styleable.GlowButton_android_text) ?: "GLOW BUTTON"
 
             recycle()
         }
-    }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-
-        val minDimensions = getDesiredDimensions()
-
-        val desiredWidth = minDimensions[0] + paddingLeft + paddingRight
-        val desiredHeight = minDimensions[1]  + backgroundPadding.times(2) + paddingTop + paddingBottom
-
-        setMeasuredDimension(getDimension(desiredWidth, widthMeasureSpec), getDimension(desiredHeight.toInt(), heightMeasureSpec))
-    }
-
-    private fun getDimension(desiredDimen: Int, measureSpec: Int): Int {
-
-        val mode = MeasureSpec.getMode(measureSpec)
-        val size = MeasureSpec.getSize(measureSpec)
-
-        d("MMM", "onMeasure " +  MeasureSpec.toString(measureSpec) + " desired " + desiredDimen);
-
-        return when(mode){
-            MeasureSpec.EXACTLY -> {
-                size
-            }
-
-            MeasureSpec.AT_MOST -> {
-                desiredDimen.coerceAtMost(size)
-            }
-
-            else -> {
-                desiredDimen
-            }
-        }
-    }
-
-    private fun getDesiredDimensions() : Array<Int> {
-
-        val textBound = Rect()
-        textPaint.textSize = mTextSize
-        textPaint.getTextBounds(text, 0, text.length, textBound)
-
-        val width = textBound.width() + backgroundPadding.times(2) + EXTRA_PADDING_WIDTH.times(2)
-        val height = textBound.height() + backgroundPadding.times(2) + EXTRA_PADDING_HEIGHT.times(2)
-
-        return arrayOf(width.toInt(), height.toInt())
     }
 
     override fun onDraw(canvas: Canvas?) {
 
-        backgroundRectF.set(backgroundPadding + paddingStart, backgroundPadding + paddingStart,
-            width.minus(backgroundPadding).minus(paddingEnd), height.minus(backgroundPadding).minus(paddingBottom))
+        //Drawing background and glow
+        backgroundRectF.set(backgroundPadding, backgroundPadding, width.minus(backgroundPadding), height.minus(backgroundPadding))
+
         with(backgroundPaint) {
             color = backColor
-            setShadowLayer(glowPadding.times(1.3f), 0f, 0f, glowColor)
+            setShadowLayer(glowRadius.times(1.3f), 0f, 0f, glowColor)
         }
 
         canvas?.drawRoundRect(backgroundRectF, cornerRadius, cornerRadius, backgroundPaint)
 
+        //Drawing text
         with(textPaint) {
-            color = textColor
+            typeface = Typeface.create(getTypeFace(), textStyle)
+            color = mTextColor
             alpha = mTextAlpha
             isLinearText = false
             textAlign = Paint.Align.CENTER
             textSize = mTextSize
-            val metrics = this.fontMetrics
-            mTextX = width.div(2).toFloat()
-            mTextY = height.div(2) - metrics.descent.plus(metrics.ascent).div(2)
-            typeface = Typeface.create(Typeface.DEFAULT, textStyle)
         }
 
         canvas?.drawText(text, mTextX, mTextY, textPaint)
 
+        //Drawing ripple effect
         if(clicked && isEnabled && rippleEnabled){
             drawRipples(canvas)
         }
     }
 
-    private fun drawRipples(canvas: Canvas?){
-        maskPath.addRoundRect(backgroundRectF, cornerRadius, cornerRadius, Path.Direction.CW)
-        canvas?.clipPath(maskPath)
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
 
-        with(ripplePaint) {
-            alpha = mRippleAlpha
-            shader = rippleGradient
+        val metrics = textPaint.fontMetrics
+        mTextX = width.div(2).toFloat() + paddingStart - paddingEnd
+        mTextY = height.div(2) - metrics.descent.plus(metrics.ascent).div(2) + paddingTop - paddingBottom
+
+        super.onLayout(changed, left, top, right, bottom)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+
+        val minDimensions = getMinDimensions()
+
+        val desiredWidth = minDimensions.width + paddingStart + paddingEnd
+        val desiredHeight = minDimensions.height + paddingTop + paddingBottom
+
+        setMeasuredDimension(getFinalDimension(desiredWidth, widthMeasureSpec),
+            getFinalDimension(desiredHeight, heightMeasureSpec))
+    }
+
+    private fun getMinDimensions(): MinimumDimensions {
+
+        val textBound = Rect()
+        with(textPaint) {
+            textSize = mTextSize
+            typeface = getTypeFace()
+            getTextBounds(text, 0, text.length, textBound)
         }
-        canvas?.drawCircle(touchX, touchY, mRippleRadius.times(3), ripplePaint)
+
+        val width = textBound.width() + backgroundPadding.times(2) +
+                horizontalTextMargin.times(2) + paddingStart + paddingEnd
+        val height = textBound.height() + backgroundPadding.times(2) +
+                verticalTextMargin.times(2) + paddingTop + paddingBottom
+
+        return MinimumDimensions(width.toInt(), height.toInt())
+    }
+
+    private fun getFinalDimension(desiredDimen: Int, measureSpec: Int): Int {
+
+        val mode = MeasureSpec.getMode(measureSpec)
+        val size = MeasureSpec.getSize(measureSpec)
+
+        return when(mode){
+            MeasureSpec.EXACTLY -> {
+                size
+            }
+            MeasureSpec.AT_MOST -> {
+                desiredDimen.coerceAtMost(size)
+            }
+            else -> {
+                desiredDimen
+            }
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -183,19 +220,112 @@ class GlowButton(context: Context, attrSet: AttributeSet?) : View(context, attrS
         if(event?.action == MotionEvent.ACTION_DOWN){
             touchX = event.x
             touchY = event.y
+
             performClick()
 
             return true
         }
-
         return false
     }
 
     override fun performClick(): Boolean {
 
-        animateRipple()
+        if(isEnabled) {
+            animateRipple()
+        }
 
         return super.performClick()
+    }
+
+    fun disable(){
+
+        isEnabled = false
+
+        mTextAlpha = 180
+        mTextColor = Color.GRAY
+        glowRadius = 0f
+
+        invalidate()
+    }
+
+    fun enable(){
+        isEnabled = true
+
+        mTextAlpha = 255
+        mTextColor = mTextColorOriginal
+        glowRadius = backgroundPadding
+
+        invalidate()
+    }
+
+    fun disableWithAnimation(){
+
+        isEnabled = false
+
+        enableDisableAnimatorSet?.cancel()
+
+        val textAlpha = ValueAnimator.ofInt(255, 180)
+        textAlpha.addUpdateListener {
+            mTextAlpha = it.animatedValue as Int
+        }
+
+        val textColor = ObjectAnimator.ofArgb(mTextColorOriginal, Color.GRAY)
+        textColor.addUpdateListener {
+            mTextColor = it.animatedValue as Int
+        }
+
+        val glow = ValueAnimator.ofFloat(glowRadius, 0f)
+        glow.addUpdateListener {
+            glowRadius = it.animatedValue as Float
+            postInvalidateOnAnimation()
+        }
+
+        enableDisableAnimatorSet = AnimatorSet()
+        with(enableDisableAnimatorSet!!){
+            duration = glowAnimationDuration
+            playTogether(glow, textColor, textAlpha)
+            start()
+        }
+    }
+
+    fun enableWithAnimation(){
+        isEnabled = true
+
+        enableDisableAnimatorSet?.cancel()
+
+        val textAlpha = ValueAnimator.ofInt(180, 255)
+        textAlpha.addUpdateListener {
+            mTextAlpha = it.animatedValue as Int
+        }
+
+        val textColor = ObjectAnimator.ofArgb(Color.GRAY, mTextColorOriginal)
+        textColor.addUpdateListener {
+            mTextColor = it.animatedValue as Int
+        }
+
+        val glow = ValueAnimator.ofFloat(0f, backgroundPadding)
+        glow.addUpdateListener {
+            glowRadius = it.animatedValue as Float
+            postInvalidateOnAnimation()
+        }
+        enableDisableAnimatorSet = AnimatorSet()
+        with(enableDisableAnimatorSet!!){
+            duration = glowAnimationDuration
+            playTogether(glow, textColor, textAlpha)
+            start()
+        }
+    }
+
+    fun setTextSize(textSize: Float){
+        mTextSize = textSize
+        requestLayout()
+    }
+
+    @JvmName("setTextColor_gb")
+    fun setTextColor(@ColorInt color: Int){
+        mTextColor = color
+        mTextColorOriginal = color
+        invalidate()
     }
 
     private fun animateRipple() {
@@ -204,7 +334,7 @@ class GlowButton(context: Context, attrSet: AttributeSet?) : View(context, attrS
 
         clicked = true
 
-        val animDuration = 1500L
+        val animDuration = rippleAnimationDuration
 
         val ripple = ValueAnimator.ofFloat(1f, 300f)
         with(ripple){
@@ -216,10 +346,8 @@ class GlowButton(context: Context, attrSet: AttributeSet?) : View(context, attrS
         }
 
         val rippleAlpha = ValueAnimator.ofInt(70, 0)
-        with(rippleAlpha) {
-            addUpdateListener {
-                mRippleAlpha = it.animatedValue as Int
-            }
+        rippleAlpha.addUpdateListener {
+            mRippleAlpha = it.animatedValue as Int
         }
 
         rippleAnimatorSet = AnimatorSet()
@@ -243,94 +371,29 @@ class GlowButton(context: Context, attrSet: AttributeSet?) : View(context, attrS
         }
     }
 
-    fun disable(){
-        mTextColorOriginal = textColor
+    private fun drawRipples(canvas: Canvas?){
+        maskPath.addRoundRect(backgroundRectF, cornerRadius, cornerRadius, Path.Direction.CW)
+        canvas?.clipPath(maskPath)
 
-        isEnabled = false
-
-        mTextAlpha = 180
-        textColor = Color.GRAY
-        glowPadding = 0f
-
-        invalidate()
+        with(ripplePaint) {
+            alpha = mRippleAlpha
+            shader = rippleGradient
+        }
+        canvas?.drawCircle(touchX, touchY, mRippleRadius.times(3), ripplePaint)
     }
 
-    fun enable(){
-        isEnabled = true
-
-        mTextAlpha = 255
-        textColor = mTextColorOriginal
-        glowPadding = backgroundPadding
-
-        invalidate()
+    private fun dpToPixel(dp: Int): Float {
+        return dp.times(resources.displayMetrics.density)
     }
 
-    fun disableWithAnimation(){
-
-        mTextColorOriginal = textColor
-
-        isEnabled = false
-
-        enableDisableAnimatorSet?.cancel()
-
-        val textAlpha = ValueAnimator.ofInt(255, 180)
-        textAlpha.addUpdateListener {
-            mTextAlpha = it.animatedValue as Int
+    private fun getTypeFace(): Typeface {
+        var tf = Typeface.DEFAULT
+        if(textFont != 0){
+            tf = ResourcesCompat.getFont(context, textFont)
         }
 
-        val textColor = ObjectAnimator.ofArgb(mTextColorOriginal, Color.GRAY)
-        textColor.addUpdateListener {
-            this.textColor = it.animatedValue as Int
-        }
-
-        val glow = ValueAnimator.ofFloat(glowPadding, 0f)
-        glow.addUpdateListener {
-            glowPadding = it.animatedValue as Float
-            postInvalidateOnAnimation()
-        }
-
-        enableDisableAnimatorSet = AnimatorSet()
-        with(enableDisableAnimatorSet!!){
-            duration = animationDuration
-            playTogether(glow, textColor, textAlpha)
-            start()
-        }
+        return tf
     }
 
-    fun enableWithAnimation(){
-        isEnabled = true
-
-        enableDisableAnimatorSet?.cancel()
-
-        val textAlpha = ValueAnimator.ofInt(180, 255)
-        textAlpha.addUpdateListener {
-            mTextAlpha = it.animatedValue as Int
-        }
-
-        val textColor = ObjectAnimator.ofArgb(Color.GRAY, mTextColorOriginal)
-        textColor.addUpdateListener {
-            this.textColor = it.animatedValue as Int
-        }
-
-        val glow = ValueAnimator.ofFloat(0f, backgroundPadding)
-        glow.addUpdateListener {
-            glowPadding = it.animatedValue as Float
-            postInvalidateOnAnimation()
-        }
-        enableDisableAnimatorSet = AnimatorSet()
-        with(enableDisableAnimatorSet!!){
-            duration = animationDuration
-            playTogether(glow, textColor, textAlpha)
-            start()
-        }
-    }
-
-//    fun setTextColor(@ColorInt color: Int){
-//        mTextColor = color
-//        mTextColorOriginal = color
-//    }
-
-    fun setTextSize(textSize: Float){
-        mTextSize = textSize
-    }
+    private data class MinimumDimensions(val width: Int, val height: Int)
 }
--- app/src/main/java/com/smb/glowbuttonsample/MainActivity.kt
+++ app/src/main/java/com/smb/glowbuttonsample/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.smb.glowbuttonsample
 
 import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.smb.glowbutton.GlowButton
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,26 +19,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val gb = GlowButton(this, null)
-//        gb.layoutParams = ViewGroup.LayoutParams(800, 300)
-//        gb.animationDuration = 1500
+        btnGoToJava.setOnClickListener {
+//            startActivity(Intent(this, JavaSample::class.java))
+        }
+
+        val gb = GlowButton(this)
+        gb.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT)
+//        gb.glowAnimationDuration = 1500
 //        gb.glowColor = Color.YELLOW
 //        gb.rippleColor = Color.CYAN
-        gb.textColor = Color.CYAN
-//        gb.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,24f, resources.displayMetrics))
-//        gb.backColor = Color.YELLOW
+//        gb.backColor = Color.MAGENTA
+//        gb.setTextColor(Color.RED)
+        gb.text = "Awesooooome"
+        gb.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,16f, resources.displayMetrics))
 //        gb.textStyle = Typeface.BOLD_ITALIC
-//        gb.cornerRadius =  TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,30f, resources.displayMetrics)
+//        gb.textFont = R.font.smile
+        gb.cornerRadius =  TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,30f, resources.displayMetrics)
 
-        viewHolder.addView(gb)
+//        viewHolder.addView(gb)
 
+
+        btnFormat.setOnClickListener {
+
+            with(btnGoToJava){
+                text = "New Text for glow"
+                glowColor = Color.RED
+                rippleColor = Color.GREEN
+                backColor = Color.YELLOW
+                textStyle = Typeface.NORMAL
+                textFont = R.font.smile
+                cornerRadius = 10f
+                setTextColor(ContextCompat.getColor(this@MainActivity, R.color.purple_200))
+                setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,30f, resources.displayMetrics))
+                glowAnimationDuration = 2000
+                rippleAnimationDuration = 5000
+            }
+        }
 
         btnEnableDisableAnimated.setOnClickListener {
             if(enabled){
                 glowButton.disableWithAnimation()
+                btnGoToJava.disableWithAnimation()
                 gb.disableWithAnimation()
             }else{
                 glowButton.enableWithAnimation()
+                btnGoToJava.enableWithAnimation()
                 gb.enableWithAnimation()
             }
             enabled = !enabled
@@ -44,6 +73,7 @@ class MainActivity : AppCompatActivity() {
             if(enabled){
                 glowButton.disable()
                 gb.disable()
+
             }else{
                 glowButton.enable()
                 gb.enable()
